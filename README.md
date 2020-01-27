a57597700a841097166ac191b6c12ece00ddf29c
MarshallPeng <marshall.s.peng.20@dartmouth.edu>
2020-01-27 01:50:45 -0500

    REINFORCE with Baseline implementation

--- README.md
+++ README.md
@@ -1,7 +1,23 @@
 ## What we did
-We are attempting to implement the tracking component of our project. 
+
+We studied and attempted to implement machine learning and reinforcement algorithms that are potentially helpful to our score-following system using PyTorch.
+
 ## Who did what
+
+### Marshall
+
+Most of my time was spent studying reinforcement learning techniques. More specifically, policy gradient methods, which is a very hot area of research currently, and is the class of algorithm that a recent paper on Score Following uses. This involved watching the lectures for (Stanford's CS234 course)[http://web.stanford.edu/class/cs234/CS234Win2019/index.html] lectures 8 and 9 on Policy search. This was then followed by reading Chapter 13 from (Sutton and Barto's 2018 Reinforcement Learning Textbook)[http://incompleteideas.net/book/RLbook2018.pdf]. Finally, I followed along a [tutorial](https://towardsdatascience.com/breaking-down-richard-suttons-policy-gradient-9768602cb63b) on how to implement one of these algorithms on OpenAI's LunarLander task.
+
 ## Learnings
+
+* PyTorch
+* Reinforcement Learning - Policy Gradient methods (REINFORCE with Baseline)
+
 ## Inspiration/Relation to project
-This comprises a significant aspect of the project in the ensemble's ability to follow along with the musician and adapt to his or her playing.
+
+The reinforcement learning aspect of this hack-a-thing is relevant to our project because we believe that, and recent research suggests that, reinforcement learning might be a better solution than the existing probablistic models that exist (and actually perform decently).
+
+
 ## What didn't work
+
+Initially, we wanted to tackle the problem of score following, but due to the short time constraint, we realized that there was no possible way that we could solve this problem for which both of us have very little experience in. Instead, we decided to dial it back and do more research/studying on potential methods and algorithms. 
---
+++ rl/policy_gradient.py
@@ -0,0 +1,141 @@
+import gym
+import numpy as np
+import torch
+
+import torch.optim as optim
+from torch.nn.functional import one_hot, log_softmax, softmax
+from collections import deque
+
+from rl.agent import Agent
+from rl.utils import Params, Utils
+
+
+# Followed tutorial/explanation from here: https://towardsdatascience.com/breaking-down-richard-suttons-policy-gradient-9768602cb63b
+
+class PolicyGradientReinforceWithBaseline:
+    def __init__(self):
+
+        self.NUM_EPOCHS = Params.NUM_EPOCHS
+        self.ALPHA = Params.ALPHA
+        self.BATCH_SIZE = Params.BATCH_SIZE
+        self.GAMMA = Params.GAMMA
+        self.HIDDEN_SIZE = Params.HIDDEN_SIZE
+        self.BETA = Params.BETA
+        self.DEVICE = torch.device('cpu')
+
+        # create the environment
+        self.env = gym.make('LunarLander-v2')
+
+        self.agent = Agent(observation_space_size=self.env.observation_space.shape[0],
+                           action_space_size=self.env.action_space.n,
+                           hidden_size=self.HIDDEN_SIZE).to(self.DEVICE)
+
+        self.adam = optim.Adam(params=self.agent.parameters(), lr=self.ALPHA)
+        self.total_rewards = deque([], maxlen=100)
+
+        self.finished_epoch = False
+
+    def solve_environment(self):
+        """
+            Implementation of REINFORCE with baseline algorithm
+        """
+        episode = 0
+        epoch = 0
+
+        epoch_logs = torch.empty(size=(0, self.env.action_space.n), device=self.DEVICE)
+        weighted_log_probs = torch.empty(size=(0,), dtype=torch.float, device=self.DEVICE)
+
+        while True:
+
+            # play an episode of the environment
+            (weighted_log_prob_trajectory,
+             episode_logits,
+             sum_of_episode_rewards,
+             episode) = self.play_episode(episode=episode)
+
+            self.total_rewards.append(sum_of_episode_rewards)
+
+            # get weighted log-probabilities of actions
+            weighted_log_probs = torch.cat((weighted_log_probs, weighted_log_prob_trajectory), dim=0)
+            epoch_logs = torch.cat((epoch_logs, episode_logits), dim=0)
+
+            if episode >= self.BATCH_SIZE:
+                self.finished_epoch = False
+                episode = 0
+                epoch += 1
+
+                # calculate the loss and update agent
+                loss, entropy = Utils.calculate_loss(epoch_logits=epoch_logs, weighted_log_probs=weighted_log_probs, beta=self.BETA)
+                self.adam.zero_grad()
+                loss.backward()
+                self.adam.step()
+
+                print("\r", f"Epoch: {epoch}, Avg Return per Epoch: {np.mean(self.total_rewards):.3f}")
+
+                # reset epoch arrays
+                epoch_logs = torch.empty(size=(0, self.env.action_space.n), device=self.DEVICE)
+                weighted_log_probs = torch.empty(size=(0,), dtype=torch.float, device=self.DEVICE)
+
+                if np.mean(self.total_rewards) > 200:
+                    print('Finished')
+                    break
+
+        self.env.close()
+
+    def play_episode(self, episode: int):
+        """
+        Run through an episode
+        """
+
+        state = self.env.reset()
+
+        # initialize the episode arrays
+        episode_actions = torch.empty(size=(0,), dtype=torch.long, device=self.DEVICE)
+        episode_logits = torch.empty(size=(0, self.env.action_space.n), device=self.DEVICE)
+        average_rewards = np.empty(shape=(0,), dtype=np.float)
+        episode_rewards = np.empty(shape=(0,), dtype=np.float)
+
+        while True:
+
+            if not self.finished_epoch:
+                self.env.render()
+
+            # get the action preferences from agent
+            action_logits, episode_logits, action = self.agent.choose_action(state, episode_logits)
+
+            episode_actions = torch.cat((episode_actions, action), dim=0)
+
+            # observe state and reward
+            state, reward, done, _ = self.env.step(action=action.cpu().item())
+
+            episode_rewards = np.concatenate((episode_rewards, np.array([reward])), axis=0)
+            average_rewards = np.concatenate((average_rewards, np.expand_dims(np.mean(episode_rewards), axis=0)), axis=0)
+
+            if done:
+
+                episode += 1
+
+                discounted_rewards_to_go = Utils.get_discounted_rewards(rewards=episode_rewards, gamma=self.GAMMA)
+                discounted_rewards_to_go -= average_rewards  # baseline
+
+                mask = one_hot(episode_actions, num_classes=self.env.action_space.n)
+
+                episode_log_probs = torch.sum(mask.float() * log_softmax(episode_logits, dim=1), dim=1)
+                episode_weighted_log_probs = episode_log_probs * \
+                    torch.tensor(discounted_rewards_to_go).float().to(self.DEVICE)
+
+                sum_weighted_log_probs = torch.sum(episode_weighted_log_probs).unsqueeze(dim=0)
+                sum_of_rewards = np.sum(episode_rewards)
+
+                self.finished_epoch = True
+
+                return sum_weighted_log_probs, episode_logits, sum_of_rewards, episode
+
+
+def main():
+    policy_gradient = PolicyGradientReinforceWithBaseline()
+    policy_gradient.solve_environment()
+
+
+if __name__ == "__main__":
+    main()
